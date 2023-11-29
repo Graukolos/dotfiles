caa65f9046781f32ccc7bdcc8c22d9dd9f71f9c0
Max Erdelmeier <graukolos@graukolos.tech>
2023-11-30 00:39:17 +0100

    Fix uuids

--- hosts/morgoth.nix
+++ hosts/morgoth.nix
@@ -6,8 +6,8 @@
   boot.initrd.kernelModules = [ "i915" "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
 
   fileSystems = {
-    "/".device = "/dev/disk/by-uuid/3bc1ba85-9526-456b-a1dc-c1544b23a7c8";
-    "/boot".device = "/dev/disk/by-uuid/9CFC-1154";
+    "/".device = "/dev/disk/by-uuid/17e7c5ec-3d6b-40bc-93cc-1cb99f90ca8d";
+    "/boot".device = "/dev/disk/by-uuid/99E2-AE73";
   };
 
   services = {
